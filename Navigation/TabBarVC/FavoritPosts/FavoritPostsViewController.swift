b13347fea1eb9aa6702c988fe7c52c11bec212ad
MihaelNevash <mihaelnevash@gmail.com>
2024-03-27 13:18:25 +0300

    + page

---
+++ Navigation/TabBarVC/FavoritPosts/FavoritPostsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritPostsViewController.swift
+//  Navigation
+//
+//  Created by Миша Вашкевич on 27.03.2024.
+//
+
+import UIKit
+
+class FavoritPostsViewController: UIViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
